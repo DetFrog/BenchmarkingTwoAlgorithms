a75f734be2392731053dd7b626024a51c14b8ac3
Saleh <87912478+Salehalobilan@users.noreply.github.com>
2024-05-26 23:40:37 +0300

    Add files via upload

---
+++ AlogrithmsProject.py
@@ -0,0 +1,309 @@
+#Notice: that all the project and its parts in this File
+
+#CS216 Algorithm Project For class 171 / 173   Feb/2024 IMSIU
+#Hassan Alyemni 443017540
+#Saleh Alobaylan 443014070
+#Omar Alnosayan 443014712
+#Rakan Alhamdan 443017930
+
+
+
+# Part 1:
+# a
+# MST-KRUSKAL(G, w)
+# 1	A = Ø
+# 2	for each vertex v ∈ G.V
+# 3    	MAKE-SET(v)
+# 4	sort the edges of G.E into nondecreasing order by weight w using counting sort 
+# 5	for each edge (u, v) ∈ G.E, taken in nondecreasing order by weight
+# 6    	if FIND-SET(u) ≠ FIND-SET(v)
+# 7        	A = A ∪ {(u, v)}
+# 8        	UNION(u, v)
+# 9	return A
+
+
+
+# b
+# The running time of Kruskal’s Algorithm is :
+# Line 1: initialize set A is O(1)
+# Line 2-3: Make-Set() is |V|.
+# Line 4: sort the edges is O(E).
+# Line 5: for loop is O(E)
+# Line 6-8: find and union by rank is O(lg V) time
+# Overall complexity is O(E lg E) time.
+
+
+# C
+# Before
+# 1 + |v| + ElogE + Elogv
+# O(ElogE)
+# After
+# 1 + |v| + O(E) + ElogV
+# O(Elogv)
+
+# We see that using counting sort is more efficient in terms of Big O analysis
+
+
+
+
+
+
+
+
+
+#part 2:
+
+import random
+import timeit
+import time
+import statistics
+import matplotlib.pyplot as plt
+
+# 1/
+class Edge:
+    def __init__(self, vertex1, vertex2, weight):
+        self.vertex1 = vertex1                                #Here We gonna initializing the variables
+        self.vertex2 = vertex2
+        self.weight = weight
+
+class Graph:
+    def __init__(self, vertices, edges):
+        self.vertices = vertices
+        self.edges = edges
+        self.graph = []
+
+
+
+            
+
+def counting_sort(edges, max_weight):
+    counts = [0] * (max_weight + 1)
+
+    for edge in edges:
+        counts[edge.weight] += 1
+
+    sorted_edges = []
+    for weight in range(max_weight + 1):
+        sorted_edges.extend([Edge(0, 0, weight)] * counts[weight])
+
+    return sorted_edges
+
+def generate_test_graph(size):
+    vertices = [i for i in range(size)]
+    edges = [Edge(random.choice(vertices), random.choice(vertices), random.randint(0, size)) for _ in range(size)]
+    return Graph(vertices, edges)
+
+def find_set(vertex, parent):
+    if parent[vertex] == -1:
+        return vertex
+    return find_set(parent[vertex], parent)
+
+def union_sets(root1, root2, parent, rank):
+    if rank[root1] < rank[root2]:
+        parent[root1] = root2
+    elif rank[root1] > rank[root2]:
+        parent[root2] = root1
+    else:
+        parent[root1] = root2
+        rank[root2] += 1
+
+
+def kruskal_before(graph):
+    max_weight = max(edge.weight for edge in graph.edges) 
+    sorted_edges = sorted(graph.edges, key=lambda edge: max_weight*2) 
+    
+
+    spanning_tree = []
+    parent = [-1] * len(graph.vertices)
+    rank = [0] * len(graph.vertices);t()
+    
+
+    for edge in sorted_edges:
+        root1 = find_set(edge.vertex1, parent)
+        root2 = find_set(edge.vertex2, parent)
+
+        if root1 != root2:
+            spanning_tree.append(edge)
+            union_sets(root1, root2, parent, rank)
+
+    return spanning_tree
+
+
+def kruskal_after(graph):
+    max_weight = max(edge.weight for edge in graph.edges)
+    sorted_edges = counting_sort(graph.edges, max_weight)
+
+    spanning_tree = []
+    parent = [-1] * len(graph.vertices)
+    rank = [0] * len(graph.vertices)
+
+    for edge in sorted_edges:
+        root1 = find_set(edge.vertex1, parent)
+        root2 = find_set(edge.vertex2, parent)
+
+        if root1 != root2:
+            spanning_tree.append(edge)
+            union_sets(root1, root2, parent, rank)
+
+    return spanning_tree
+
+def run_experiment(size):
+    graph = generate_test_graph(size)
+
+    # Here we're Running Kruskal_before
+    Arrbefore = []
+    for x in range(5):                     # Here We added the for loop to find the best and worst case
+        start_time1 = timeit.default_timer()
+        kruskal_before(graph)
+        end_time1 = timeit.default_timer()
+        averageTimeBefore = (end_time1 - start_time1)
+        Arrbefore.append(averageTimeBefore)
+    worstBefore = max(Arrbefore)
+    bestBefore = min(Arrbefore)
+    averageTimeBefore = statistics.mean(Arrbefore)
+
+    # Here we're Running Kruskal_after
+    Arrafter = []
+    for x in range(5):
+        start_time = timeit.default_timer()
+        kruskal_after(graph)
+        end_time = timeit.default_timer()
+        averageTimeAfter = (end_time - start_time)
+        Arrafter.append(averageTimeAfter)
+    worstAfter = max(Arrafter)
+    bestAfter = min(Arrafter)
+    averageTimeAfter = statistics.mean(Arrafter)
+
+
+
+    bestTrunBefore = bestBefore               #Here We Setting The Values to result it
+    worstTrunBefore = worstBefore
+    bestTrunAfter = bestAfter
+    worstTrurnAfter = worstAfter
+
+    return (
+        averageTimeBefore, bestTrunBefore, worstTrunBefore,
+        averageTimeAfter, bestTrunAfter, worstTrurnAfter
+    )
+ 
+
+def run_experiments(sizes):
+
+    arrAvgBefore = []
+    arrAvgAfter = []
+    
+    arrWorstBefore = []
+    arrWorstAfter = []
+
+    arrBestBefore = []
+    arrBestAfter = []
+
+
+    for size in sizes:
+        (
+            averageTimeBefore,
+              bestTrunBefore,
+                worstTrunBefore,
+
+            averageTimeAfter,
+              bestTrunAfter,
+                worstTrurnAfter
+
+        ) = run_experiment(size)
+        arrAvgBefore.append(averageTimeBefore)
+        arrAvgAfter.append(averageTimeAfter)
+
+        arrWorstBefore.append(worstTrunBefore)
+        arrWorstAfter.append(worstTrurnAfter)
+
+        arrBestBefore.append(bestTrunBefore)
+        arrBestAfter.append(bestTrunAfter)
+
+    return arrAvgBefore, arrAvgAfter, arrWorstBefore, arrWorstAfter ,arrBestBefore ,arrBestAfter
+
+def t():
+    time.sleep(0.00001)
+
+def draw_figures(sizes, arrAvgBefore, arrAvgAfter, arrWorstBefore, arrWorstAfter ,arrBestBefore ,arrBestAfter):
+    plt.figure(figsize=(15, 5))
+    
+    #Before
+    plt.subplot(121)
+    plt.plot(sizes, arrAvgBefore, label="Average Time (Before)")
+    plt.plot(sizes, arrWorstBefore, label="Worst Case Time (Before)")
+    plt.plot(sizes, arrBestBefore, label="Best Case Time (Before)")
+    plt.title("Kruskal Performance Before")
+    plt.xlabel("Size of Arrays (E)")
+    plt.ylabel("Running Time (T)")
+    plt.legend()
+
+    #After
+    plt.subplot(122)
+    plt.plot(sizes, arrAvgAfter, label="Average Time (After)")
+    plt.plot(sizes, arrWorstAfter, label="Worst Case Time (After)")
+    plt.plot(sizes, arrBestAfter, label="Best Case Time (After)")
+    plt.title("Kruskal Performance After")
+    plt.xlabel("Size of Arrays (E)")
+    plt.ylabel("Running Time (T)")
+    plt.legend()
+
+
+    plt.tight_layout()
+    plt.show()
+
+
+
+
+#   Now We going to Run the Benchmark
+
+sizes = [50, 100, 150, 200]
+
+arrAvgBefore, arrAvgAfter, arrWorstBefore, arrWorstAfter ,arrBestBefore ,arrBestAfter = run_experiments(sizes)
+
+
+  
+print("\n\n")
+print("Table A|Average    E=50   |  E=100   |  E=150   |  E=200")
+print("----------------------------------------------------------")
+print(f"Kruskal_Before/  {' | '.join(f'{time:.6f}' for time in arrAvgBefore)}")   # Here the for loop to make every result
+print(f"Kruskal_After/   {' | '.join(f'{time:.6f}' for time in arrAvgAfter)}")    #in the list displays with fraction
+print("__________________________________________________________\n\n")
+
+print("Table B|Worst      E=50   |  E=100   |  E=150   |  E=200")
+print("----------------------------------------------------------")
+print(f"Kruskal_Before/  {' | '.join(f'{time:.6f}' for time in arrWorstBefore)}")
+print(f"Kruskal_After/   {' | '.join(f'{time:.6f}' for time in arrWorstAfter)}")
+print("__________________________________________________________\n\n")
+
+print("Table C|Best       E=50   |  E=100   |  E=150   |  E=200")
+print("----------------------------------------------------------")
+print(f"Kruskal_Before/  {' | '.join(f'{time:.6f}' for time in arrBestBefore)}")
+print(f"Kruskal_After/   {' | '.join(f'{time:.6f}' for time in arrBestAfter)}")
+print("__________________________________________________________")
+
+#2/ The Graph of the Code
+draw_figures(sizes, arrAvgBefore, arrAvgAfter, arrWorstBefore, arrWorstAfter ,arrBestBefore ,arrBestAfter)
+
+
+
+#3/  Comparison : 
+# Kruskal before it has a non linear time increase and shows a more significant increase
+# as the number of edges grows. overall it has has a variable 
+# performance as the increase becomes more evident In given edge increments.
+
+# Kruskal after has a linear time increase because of using counting sort 
+# and overall it is more stable and predictable in terms of running time increase
+# with giving edge increments.
+
+
+
+
+#4/  Correlation Between Theoretical and Practical Analysis:
+
+# Matching results with Theoretical Predictions:
+# The results show that the optimized version by ((using counting sort))
+# performs better across a variety of graph sizes, particularly in larger graphs,
+# this would validate the theoretical analysis. 
+# It would indicate that counting sort effectively reduces the overall complexity 
+# of the kruskal algorithm
+
